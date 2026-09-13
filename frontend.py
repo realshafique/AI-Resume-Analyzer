58645355f2731854770d492a2cd9a043031956f2
real_shafique <shafiqurrahmanansari0786@gmail.com>
2026-09-13 14:08:29 +0530

    Configure API URL for deployment

--- frontend.py
+++ frontend.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import requests
+import os
 
-
-API_URL = "http://127.0.0.1:8000"
-
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000"
+)
 
 st.set_page_config(
     page_title="AI Resume Analyzer",
